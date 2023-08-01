0565a8a9668abcf21ea0a0b3269a428abee99bea
Icebeard <nexts.django@gmail.com>
2023-08-01 22:21:36 +0200

    main app first version

---
+++ emailproject/mailer/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+
+def sendanemail(request):
+    if request.method == "POST":
+        to = request.POST.get('toemail')
+        content = request.POST.get('content')
+        send_mail(
+            #subject
+            "testing",
+            #message
+            content,
+            #from email
+            settings.EMAIL_HOST_USER,
+            #recipent list
+            [to]
+        )
+        return render(
+        request,
+        'email.html',
+        {
+        'title':'send an email'
+        }
+    )
+    else:
+        return render(
+        request,
+        'email.html',
+        {
+        'title':'send an email'
+        }
+    )
